dc99ba0eaebaac3ec4fd0d9e817b55c7c1c02d36
mikera <mike.r.anderson.13@gmail.com>
2012-09-28 17:28:21 +0800

    Remove old Point3D, update tests

--- src/test/java/enlight/TestPoints.java
+++ src/test/java/enlight/TestPoints.java
@@ -3,30 +3,28 @@ package enlight;
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
-import enlight.maths.Point3D;
+import mikera.vectorz.Vector3;
 
 public class TestPoints {
 	@Test public void testPoint3D() {
-		assertEquals(new Point3D(1,1,1), new Point3D(1,1,1));
+		assertEquals(new Vector3(1,1,1), new Vector3(1,1,1));
 		
-		Point3D a=new Point3D();
-		assertEquals(new Point3D(0,0,0),a);
-		a.set(1,2,3);
-		assertEquals(new Point3D(1,2,3), a);
+		Vector3 a=new Vector3();
+		assertEquals(new Vector3(0,0,0),a);
+		a.setValues(1,2,3);
+		assertEquals(new Vector3(1,2,3), a);
 		
-		a.set(3,4,0);
-		assertEquals(5.0,a.length(),0.0001);
-		a.normalize();
+		a.setValues(3,4,0);
+		assertEquals(5.0,a.magnitude(),0.0001);
+		a.normalise();
 		assertEquals(0.6,a.x,0.0001);
 		assertEquals(0.8,a.y,0.0001);
 		assertEquals(0.0,a.z,0.0001);
 		
-		a.set(1,2,3);
-		assertEquals(14.0,a.lengthSquared(),0.0001);
+		a.setValues(1,2,3);
+		assertEquals(14.0,a.magnitudeSquared(),0.0001);
 		a.add(-2,-2,-2);
-		assertEquals(2.0,a.lengthSquared(),0.0001);
-
-		
+		assertEquals(2.0,a.magnitudeSquared(),0.0001);
 		
 	}
 }
