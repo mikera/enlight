a5bc51d14d6e56ddefdaf7a9a99b0903f6a75d4d
mikera <mike.r.anderson.13@gmail.com>
2012-10-09 16:03:53 +0800

    Misc edits

--- src/main/java/enlight/Keys.java
+++
@@ -1,7 +0,0 @@
-package enlight;
-
-import clojure.lang.Keyword;
-
-public class Keys {
-	public Keyword CAMERA=Keyword.intern(null, "camera");
-}
--- src/main/java/enlight/geom/ASceneObject.java
+++ src/main/java/enlight/geom/ASceneObject.java
@@ -1,5 +1,10 @@
 package enlight.geom;
 
+/**
+ * Abstract base class for all scene objects
+ * 
+ * @author Mike
+ */
 public abstract class ASceneObject implements ISceneObject {
 	
 	public boolean isFinite() {
