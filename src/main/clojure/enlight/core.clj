2b8df0d114b88a20849d7de3e9fa03a02a678923
mikera <mike.r.anderson.13@gmail.com>
2012-12-08 07:22:51 +0800

    Add test namespace for core

--- src/main/clojure/enlight/core.clj
+++ src/main/clojure/enlight/core.clj
@@ -1,2 +1,10 @@
-(ns enlight.core)
+(ns enlight.core
+  (import [mikera.image]))
 
+(defn render 
+  "Render a scene to a new bufferedimage"
+  ([scene
+              & {:keys [width height] 
+                 :or {width 256 height 256}}]
+  (let [b nil]
+    b)))
---
+++ src/test/clojure/enlight/test_core.clj
@@ -0,0 +1,10 @@
+(ns enlight.test-core
+  (:use [enlight core])
+   (:use [clojure test]))
+
+
+
+(deftest test-render
+  (testing "Basic render"
+    (let [b nil]
+      )))
