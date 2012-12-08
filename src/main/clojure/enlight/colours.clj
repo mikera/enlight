c8b54b06ff68c57fb5ce703e6ee7825fb186f866
mikera <mike.r.anderson.13@gmail.com>
2012-12-09 04:11:08 +0800

    Add colours.clj namespace

---
+++ src/main/clojure/enlight/colours.clj
@@ -0,0 +1,14 @@
+(ns enlight.colours
+  (import [mikera.vectorz Vector3 Vector4 AVector Vectorz]))
+
+;; utility functions for treating vectors as colours
+
+(defn rgb-from-vector 
+  "Converts a colour vector into an ARGB colour value"
+  (^long [^Vector4 colour]
+    (mikera.image.Colours/getRGBClamped (.get colour 0) (.get colour 1) (.get colour 2))))
+
+(defn argb-from-vector4 
+  "Converts a colour vector into an ARGB colour value"
+  (^long [^Vector4 colour]
+    (mikera.image.Colours/getARGBClamped (.x colour) (.y colour) (.z colour) (.t colour))))
