cdca539625624efe1ea551a64dd58d80e3ee241b
mikera <mike.r.anderson.13@gmail.com>
2012-12-09 09:47:07 +0800

    Widen cljunit testing to all namespaces

--- src/test/java/enlight/ClojureTests.java
+++ src/test/java/enlight/ClojureTests.java
@@ -1,15 +1,7 @@
 package enlight;
 
-import java.util.Arrays;
-import java.util.List;
-
 import mikera.cljunit.ClojureTest;
 
 public class ClojureTests extends ClojureTest {
-	@Override
-	public List<String> namespaces() {
-		return Arrays.asList(new String[] {
-			"enlight.test-core"
-		});
-	}
+	// default: do all clojure namespace tests
 }
