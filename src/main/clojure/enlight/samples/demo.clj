994062e8f6e6c391f1dda47a6d23b1528dfd55e5
mikera <mike.r.anderson.13@gmail.com>
2012-12-10 06:15:46 +0800

    Demo examples

--- src/main/clojure/enlight/samples/demo.clj
+++ src/main/clojure/enlight/samples/demo.clj
@@ -10,4 +10,12 @@
   {:camera {:position [0 0 -5]
             :direction [0 0 1]
             :up [0 1 0]
-            :right [1 0 0]}})
+            :right [1 0 0]}
+   
+   })
+
+
+(defn demo []
+  (show EXAMPLE-SCENE)
+  
+)
