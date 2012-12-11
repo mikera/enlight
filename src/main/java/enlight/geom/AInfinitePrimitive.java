c8bb66a144c728496dce12d7f43d18fc9176039b
mikera <mike.r.anderson.13@gmail.com>
2012-12-11 17:04:28 +0800

    Add Sky Sphere primitive

--- src/main/java/enlight/geom/AInfinitePrimitive.java
+++ src/main/java/enlight/geom/AInfinitePrimitive.java
@@ -1,9 +1,15 @@
 package enlight.geom;
 
+import mikera.vectorz.Vector3;
+
 public abstract class AInfinitePrimitive extends APrimitive {
 	@Override
 	public boolean isFinite() {
 		return false;
 	}
 
+	@Override
+	public void getSupport(Vector3 normal, IntersectionInfo resultOut) {
+		throw new UnsupportedOperationException("Support vector calculation not possible on infinite primitives!!!");
+	}
 }
---
+++ src/main/java/enlight/geom/primitive/SkySphere.java
@@ -0,0 +1,18 @@
+package enlight.geom.primitive;
+
+import mikera.vectorz.Vector3;
+import enlight.geom.AInfinitePrimitive;
+import enlight.geom.IntersectionInfo;
+
+public class SkySphere extends AInfinitePrimitive {
+
+
+
+	@Override
+	public void getIntersection(Vector3 start, Vector3 direction, double dist,
+			IntersectionInfo result) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
