5c76809f880e643596e69fd1842599db57958dcb
mikera <mike.r.anderson.13@gmail.com>
2012-12-13 13:54:32 +0800

    Use latest vectorz-clj

--- src/main/java/enlight/model/Utils.java
+++ src/main/java/enlight/model/Utils.java
@@ -2,6 +2,7 @@ package enlight.model;
 
 import mikera.transformz.ATransform;
 import mikera.transformz.Transformz;
+import mikera.transformz.impl.IdentityTranslation;
 import mikera.vectorz.Vector3;
 import mikera.vectorz.geom.BoundBox;
 
@@ -13,7 +14,7 @@ public class Utils {
 	public static final Vector3 Y_NEGATIVE=Vector3.of(0,-1,0);
 	public static final Vector3 Z_NEGATIVE=Vector3.of(0,0,-1);
 	
-	public static final ATransform IDENTITY_3D=Transformz.identityTransform(3);
+	public static final IdentityTranslation IDENTITY_3D=Transformz.identityTranslation(3);
 	
 	public static final ATransform DEFAULT_RGB_FUNCTION=Transformz.constantTransform(3, Vector3.of(1,1,1));
 
