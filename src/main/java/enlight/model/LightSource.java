a8ae0e687b32246f6f0f4490dfd30fc2d154aa13
mikera <mike.r.anderson.13@gmail.com>
2012-12-30 08:53:04 +0800

    Add light source position field

--- src/main/java/enlight/model/LightSource.java
+++ src/main/java/enlight/model/LightSource.java
@@ -3,28 +3,29 @@ package enlight.model;
 import java.util.HashMap;
 import java.util.Map;
 
-import mikera.transformz.ATransform;
 import mikera.vectorz.Vector3;
 import mikera.vectorz.geom.BoundBox;
 import mikera.vectorz.geom.Ray;
 import clojure.lang.Keyword;
 import enlight.Colour;
 import enlight.Key;
-import enlight.model.primitive.APrimitive;
 
 public class LightSource extends ASceneObject {
 	public final boolean shadowless;
 	public final Vector3 colour;
+	public final Vector3 position;
 		
 	public LightSource () {
 		shadowless=false;
 		colour=Colour.WHITE;
+		position=Vector3.of(0,0,0);
 	}
 	
 	protected LightSource(LightSource old, Map<Object, Object> props) {
 		super(old,props);
 		shadowless=(Boolean) (props.containsKey(Key.SHADOWLESS)?props.get(Key.SHADOWLESS):old.shadowless);
 		colour=(Vector3) (props.containsKey(Key.COLOUR)?props.get(Key.COLOUR):old.colour);
+		position=(Vector3) (props.containsKey(Key.POSITION)?props.get(Key.POSITION):old.position);
 	}
 	
 	@Override
