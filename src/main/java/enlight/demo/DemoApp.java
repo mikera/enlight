b3c5e0776a03925c6d93f81e4e2704241c399faf
mikera <mike.r.anderson.13@gmail.com>
2013-01-02 16:33:56 +0800

    Demo app update

--- src/main/java/enlight/demo/DemoApp.java
+++ src/main/java/enlight/demo/DemoApp.java
@@ -4,6 +4,8 @@ import mikera.cljutils.Clojure;
 
 public class DemoApp {
 	public static void main(String[] args) {
-		Clojure.eval("(do (require 'enlight.samples.demo) (enlight.core/show enlight.samples.demo/EXAMPLE-SCENE))");
+		Clojure.eval("(do " +
+				        "(require 'enlight.samples.demo) " +
+				        "(enlight.core/show enlight.samples.demo/EXAMPLE-SCENE))");
 	}
 }
